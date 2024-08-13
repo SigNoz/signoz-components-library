43387490a34f52dc97fc8fecc8ad25c8e7701a7e
Yunus M <myounis.ar@live.com>
2024-08-13 14:03:11 +0530

    feat: add chromatic

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,13 +1,11 @@
 import { dirname, join } from "path";
 module.exports = {
   stories: ["../src/**/*.stories.@(ts|tsx|js|jsx)"],
-
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("storybook-css-modules")
   ],
-
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {
@@ -16,16 +14,13 @@ module.exports = {
       },
     },
   },
-
   typescript: {
     reactDocgen: "react-docgen-typescript"
   },
-
   docs: {
-    autodocs: true
+    autodocs: false
   }
 };
-
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
